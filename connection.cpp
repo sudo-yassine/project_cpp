2ead84cd3b35e361e74b29e3043c32291a9cf1fc
mahcharfaoui <mohamedmehdi.charfaoui@esprit.tn>
2023-12-01 10:33:03 +0100

    Gestion Client

---
+++ connection.cpp
@@ -0,0 +1,23 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection:: createconnection()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Projet2A");
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("systeme");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
